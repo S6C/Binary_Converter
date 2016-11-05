9abd418317f5668f9482087335337bf456f857d5
Craig Chambers <cchambers@salisbury6c.ac.uk>
2016-11-05 15:25:29 +0000

    PEP8 changes

--- bc.py
+++ bc.py
@@ -3,22 +3,22 @@ num = input('What is the decimal number to convert? ')
 
 # Test that we can convert to an integer
 try:
-  d = int(num)
+    d = int(num)
 except:
-  print('Please enter a decimal number')
+    print('Please enter a decimal number')
 
 # empty string to store binary number
 b = ''
 
-#while loop keeps dividing until number is 0
+# while loop keeps dividing until number is 0
 while d > 0:
-  r = d % 2 # Gets the remainder
-  b = b + str(r) # Adds the remainder to the empty string
-  d = int(d/2) # Gets how many times the number can be divided by two (but not the remainder)
-  
+    r = d % 2  # Gets the remainder
+    b = b + str(r)  # Adds the remainder to the empty string
+# Gets how many times the number can be divided by two (but not the remainder)
+    d = int(d / 2)
+
 # reverse b so it reads correct
 b = b[::-1]
 
-#print the answer
+# print the answer
 print(str(num) + ' in binary is ' + str(b))
-
